ecaf01224394a892a18f60da1bf3cdb6797a32c6
Charlie <charles.mulic@gmail.com>
2018-12-08 07:44:49 -0600

    implement map based services, closes #6

---
+++ pet-clinic-data/src/main/java/net/cilution/sfgpetclinic/services/map/PetServiceMap.java
@@ -0,0 +1,33 @@
+package net.cilution.sfgpetclinic.services.map;
+
+import net.cilution.sfgpetclinic.model.Pet;
+import net.cilution.sfgpetclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+}
