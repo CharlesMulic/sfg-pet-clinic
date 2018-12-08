d8a01857f718cd540f7f94af4e4eac235f7ee000
Charlie <charles.mulic@gmail.com>
2018-12-08 03:59:31 -0600

    implement service interfaces. closes #5

---
+++ pet-clinic-data/src/main/java/net/cilution/sfgpetclinic/services/VetService.java
@@ -0,0 +1,14 @@
+package net.cilution.sfgpetclinic.services;
+
+import net.cilution.sfgpetclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
