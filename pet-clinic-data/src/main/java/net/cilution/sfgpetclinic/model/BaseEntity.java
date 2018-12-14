87abd6b0609ea18056d324034342b1ccbaed9327
Charlie <charles.mulic@gmail.com>
2018-12-14 07:51:05 -0600

    implement base entity. closes #35

--- pet-clinic-data/src/main/java/net/cilution/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/net/cilution/sfgpetclinic/model/BaseEntity.java
@@ -1,9 +1,13 @@
 package net.cilution.sfgpetclinic.model;
 
+import javax.persistence.*;
 import java.io.Serializable;
 
+@MappedSuperclass // base class for jpa, don't need this class mapped to database
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
