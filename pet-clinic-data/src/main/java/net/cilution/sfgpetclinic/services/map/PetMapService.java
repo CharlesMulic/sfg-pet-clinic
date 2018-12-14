c6f2866d99477b8adf69ad5ed6ab96bb340a1e85
Charlie <charles.mulic@gmail.com>
2018-12-14 09:15:19 -0600

    updated profiles for jpa service. closes #48

--- pet-clinic-data/src/main/java/net/cilution/sfgpetclinic/services/map/PetMapService.java
+++ pet-clinic-data/src/main/java/net/cilution/sfgpetclinic/services/map/PetMapService.java
@@ -2,11 +2,13 @@ package net.cilution.sfgpetclinic.services.map;
 
 import net.cilution.sfgpetclinic.model.Pet;
 import net.cilution.sfgpetclinic.services.PetService;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
 @Service
+@Profile({"default", "map"})
 public class PetMapService extends AbstractMapService<Pet, Long> implements PetService {
     @Override
     public Set<Pet> findAll() {
